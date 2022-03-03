3c4a57d1588fb95e1d7628aafdfe06b1d31ba641
Eddsalasgit <edslgn.1234@gmail.com>
2022-03-03 10:50:54 -0600

    entrar

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,9 @@ const Home = () => {
   const user = supabaseClient.auth.user();
 
   useEffect(() => {
+    if (!user) {
       router.push("/signin");
+    }
   }, [user, router]);
 
   useEffect(() => {
